aa4a4f50ada7dbe447dfbaf3e1c00cee7196cd24
mib7514 <58714696+mib7514@users.noreply.github.com>
2026-03-08 14:28:18 +0900

    Create layout.js

---
+++ app/modules/deficit-analysis/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "적자기업 투자분석 | 늑대무리원정단",
+  description: "코스닥 시총 Top50 적자기업 유형분류 및 위험대비 기대수익 분석",
+};
+
+export default function DeficitAnalysisLayout({ children }) {
+  return children;
+}
